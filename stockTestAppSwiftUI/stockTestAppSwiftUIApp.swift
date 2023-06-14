3c3f0324d5d021591d43b3d7271a08d344496c13
Denys Havryliak <den4iklvivua@gmail.com>
2023-06-14 14:36:42 +0300

    Initial Commit

---
+++ stockTestAppSwiftUI/stockTestAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  stockTestAppSwiftUIApp.swift
+//  stockTestAppSwiftUI
+//
+//  Created by Denys on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct stockTestAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
